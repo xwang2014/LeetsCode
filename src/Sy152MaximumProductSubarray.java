4dec779461add34b7137b0746766eff1e0adbcf7
ShuaiYe1988 <yeshuaiosu@gmail.com>
2015-02-15 23:20:26 -0500

    shuai ye commit 152 and 153

---
+++ src/Sy152MaximumProductSubarray.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public int maxProduct(int[] A) {
+        if(A==null||A.length==0)
+            return 0;
+        if(A.length==1)
+            return A[0];
+        
+        int max = A[0];
+        int localMax = A[0];
+        int localMin = A[0];
+        
+        for(int i=1; i<A.length; i++){
+            
+            if(A[i]==0){
+                
+                localMax = 1;
+                localMin = 1;
+                if(A[i]>max)
+                    max = A[i];
+            }
+            else{
+                int temp = localMax;
+                localMax = Math.max(Math.max(localMax*A[i], localMin*A[i]), A[i]);
+                localMin = Math.min(Math.min(temp*A[i], localMin*A[i]), A[i]);
+           
+                if(localMax > max)
+                    max = localMax;
+            }
+        }
+        
+        return max;
+    }
+}
---
+++ src/Sy153FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public int findMin(int[] num) {
+        if(num==null||num.length==0)
+            return 0;
+        if(num.length==1)
+            return num[0];
+            
+        //log(n)
+        return findMin(num, 0, num.length-1);
+    }
+    
+    public int findMin(int[] num, int start, int end){
+        
+        if(start == end)
+            return num[start];
+        if(start>end)
+            return Integer.MAX_VALUE;
+        int mid = (start + end)/2;
+        
+        int min = 0;
+        if(num[mid]>num[start]){
+            
+            min = num[start];
+            min = Math.min(min, findMin(num, mid+1,end));
+        }else if(num[mid]<num[start]){
+            
+            min = num[mid];
+            min = Math.min(min, findMin(num, start, mid-1));
+        }else {
+            
+            min = Math.min(num[start], num[end]);
+        }
+        
+        return min;
+    }
+
+}
